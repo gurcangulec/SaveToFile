ff3746cf20f29bbbb59fbb46ffc197f32d22207e
Gürcan Güleç <gurcanglc@gmail.com>
2022-09-12 19:34:27 +0300

    Initial Commit

---
+++ SaveToFile/SaveToFileApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveToFileApp.swift
+//  SaveToFile
+//
+//  Created by Gürcan Güleç on 12.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SaveToFileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
