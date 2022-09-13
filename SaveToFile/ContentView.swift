9f1d52ff10cbd2f6253f958d39ce8de5b03cc231
Gürcan Güleç <gurcanglc@gmail.com>
2022-09-13 14:38:19 +0300

    Initial commit

--- SaveToFile/ContentView.swift
+++ SaveToFile/ContentView.swift
@@ -8,9 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var entry = String()
+    let savePath = FileManager.documentsDirectory.appendingPathComponent("Saved.txt")
+    
+    init() {
+        print("Init started")
+        print("entry: \(entry)")
+        do {
+            let data = try Data(contentsOf: savePath)
+            entry = try JSONDecoder().decode(String.self, from: data)
+            print("doing")
+            print(entry)
+        } catch {
+            entry = String()
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Button("See") {
+                print("entry: \(entry)")
+            }
+            Button("Save") {
+                save()
+            }
+            Button("Write data") {
+                write()
+            }
+        }
+    }
+    
+    func save() {
+        do {
+            print(entry)
+            let data = try JSONEncoder().encode(entry)
+            try data.write(to: savePath, options: [.atomicWrite, .completeFileProtection])
+            print("saved")
+        } catch {
+            print("Unable")
+        }
+    }
+    
+    func write() {
+        entry = "Something"
     }
 }
 
